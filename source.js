b3b4a0636dd74d39de9af2caad814d8361859a22
ruvin@simct.com <Empirestate162#>
2021-05-30 14:57:31 +0530

    first commit

---
+++ source.js
@@ -0,0 +1,145 @@
+const socket = io.connect("https://rtc-test-server.herokuapp.com/");
+console.log(socket);
+const peerConnections = {};
+let captureStream = null;
+ var client;
+var Tvideo = document.getElementById('videocontent');
+
+
+function onBroadcast() {
+ 
+  socket.emit('broadcaster');
+  console.log("video started");
+  
+   //Connect Options
+      var options = {
+        timeout: 3,
+        //Gets Called if the connection has sucessfully been established
+        onSuccess: function () {
+          console.log("Connected");
+        },
+        //Gets Called if the connection could not be established
+        onFailure: function (message) {
+          console.log("Connection failed: " + message.errorMessage);
+        }
+      };
+
+      client.connect(options);
+  
+  
+  
+};
+
+function onScreenShare() {
+ startCapture(Tvideo);
+ client = new Messaging.Client("broker.mqttdashboard.com", 8000, "myclientid_ruvin123");
+ 
+ 
+  //Gets  called if the websocket/mqtt connection gets disconnected for any reason
+      client.onConnectionLost = function (responseObject) {
+        //Depending on your scenario you could implement a reconnect logic here
+       console.log("connection lost: " + responseObject.errorMessage);
+      };
+    
+ 
+ 
+ 
+};
+
+function onSend(){
+	
+    var x = document.getElementById("myTextarea").value;
+    dataChannelB.send(x);	
+};
+
+
+const config = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302"]
+    }
+  ]
+};
+ var dataChannelA;
+ var dataChannelB;
+
+//creat broadcasters sdp and emit
+socket.on('watcher', function (id) {
+  const peerConnection = new RTCPeerConnection(config);
+  peerConnections[id] = peerConnection;
+  dataChannelA = peerConnection.createDataChannel("channelA", {negotiated: true, id: 1});
+  dataChannelB = peerConnection.createDataChannel("channelB", {negotiated: true, id: 2});
+  peerConnection.addStream(Tvideo.captureStream());
+  peerConnection.createOffer()
+      .then(sdp => peerConnection.setLocalDescription(sdp))
+      .then(function () {
+          socket.emit('offer', id, peerConnection.localDescription);
+		  console.log("emit offer");
+      });
+  peerConnection.onicecandidate = function (event) {
+      if (event.candidate) {
+          socket.emit('candidate', id, event.candidate);
+      }
+  };
+  
+  	
+    dataChannelA.onopen = function (event) {
+      console.log("Data channel A ready");  
+  };
+   dataChannelB.onopen = function (event) {
+      console.log("Data channel B ready ");  
+  };
+   dataChannelA.onmessage = function(event){
+      console.log(event.data);
+	  
+      let topic = "testtopic/laser/pos";
+      let qos = 2;
+
+      var message = new Messaging.Message(event.data);
+      message.destinationName = topic;
+      message.qos = qos;
+      client.send(message);
+    
+      };
+	  dataChannelB.onmessage = function(event){
+      console.log("chanel B "+event.data);     
+      document.getElementById("recieveMsg").value += event.data + '\r\n';
+      };
+
+     });
+	
+
+
+//get clients sdp
+socket.on('answer', function (id, description) {
+  peerConnections[id].setRemoteDescription(description);
+});
+
+socket.on('candidate', function (id, candidate) {
+  peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+});
+
+socket.on('bye', function (id) {
+  peerConnections[id] && peerConnections[id].close();
+  delete peerConnections[id];
+  client.disconnect();
+});
+
+var displayMediaOptions = {
+   video: {
+     cursor: "always"
+   },
+   audio: false
+ };
+async function startCapture(Tvideo) {
+
+
+   try {
+     captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+     Tvideo.srcObject=captureStream;
+ 
+   } catch(err) {
+     console.error("Error: " + err);
+   }
+   return captureStream;
+ }
